331e8668d4752d35f72ab1fe2c0ee0de438f3edf
Megan McCaw <meganm.mccaw@gmail.com>
2024-09-01 15:14:18 -0400

    added return_negative kata

---
+++ 8kyu/return_negative.js
@@ -0,0 +1,17 @@
+// In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+
+// Examples
+// makeNegative(1);    // return -1
+// makeNegative(-5);   // return -5
+// makeNegative(0);    // return 0
+// makeNegative(0.12); // return -0.12
+// Notes
+// The number can be negative already, in which case no change is required.
+// Zero (0) is not checked for any specific sign. Negative zeros make no mathematical sense.
+
+// Pseudocode
+// Take in a number that is either positive or already negative
+// Return the negative value no matter if the number is positive/negative
+
+
+makeNegative = num => num > 0 ? num * -1 : num
